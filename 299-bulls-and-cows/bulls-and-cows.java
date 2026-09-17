44c2faf1d474ccf6ee4bb18f8f32c8ecfd03c7ce
durka25devi <durka0425@gmail.com>
2026-09-17 21:12:52 +0530

    Time: 3 ms (95.60%) | Memory: 43.3 MB (87.00%) - LeetSync

---
+++ 299-bulls-and-cows/bulls-and-cows.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String getHint(String secret, String guess) {
+        int s[] =new int[10];
+        int g[]=new int[10];
+        int n=secret.length();
+        int bulls=0;
+        int cows=0;
+
+        for(int i=0;i<n;i++){
+            if(secret.charAt(i)==guess.charAt(i)){
+                bulls++;
+            }
+            else{
+                s[secret.charAt(i)-'0']++;
+                g[guess.charAt(i)-'0']++;
+            }
+        }
+
+        for(int i=0;i<10;i++){
+            cows+=Math.min(s[i],g[i]);
+        }
+        return bulls + "A" + cows +"B";
+        
+    }
+}
